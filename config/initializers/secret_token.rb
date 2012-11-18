88c5d25631d2187dfb9d8ebfa4043b5ba7127b3c
Dalto <dalto.perreault@gmail.com>
2012-11-18 12:50:26 -0500

    Added rails file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WipeMyTimeline::Application.config.secret_token = '6d1cc5ffafa6fa295a2cf31f18b36ad03ac7e1246e6374327af1fbff2c3b0995b26e7ec89b7e42ae307c35d74458b7c70ad0ae4492c0e161f74cb81d04fa9468'

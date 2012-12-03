9fc1de963d38e5a15be837707e3f3263a01e4748
Dalto <dalto.perreault@gmail.com>
2012-12-03 12:52:42 -0500

    Added a current_user fonction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+
+	def current_user
+		@current_user ||= getFBUser(session[:access_token])
+	end
+
+	helper_method :current_user
+
+	protected
+
+	def getFBUser(access_token)
+		if access_token
+			return FbGraph::User.new('me', :access_token => access_token['credentials']['token']).fetch		
+		end
+	end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,4 @@
 class HomeController < ApplicationController
 	def index
-		
-		if session[:access_token]
-			@user = session[:access_token]
-		end
 	end
 end
